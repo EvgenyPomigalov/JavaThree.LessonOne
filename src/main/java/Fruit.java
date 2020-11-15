3d9476ef1e9c356feffe327fcc25ee603179c7ad
EvgenyPomigalov <eugene-2005@yandex.ru>
2020-11-15 20:37:04 +0900

    Final

---
+++ src/main/java/Fruit.java
@@ -0,0 +1,15 @@
+public class Fruit {
+    private float weigth;
+
+    public Fruit(float weigth) {
+        this.weigth = weigth;
+    }
+
+    public float getWeigth() {
+        return weigth;
+    }
+
+    public void setWeigth(float weigth) {
+        this.weigth = weigth;
+    }
+}
